7f47926609393f219fd550c372187ef2e5c9370b
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-09-28 03:12:30 +0100

    ...sponsors section

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -3,7 +3,7 @@ import purpleStar from "../assets/purpleStar.png";
 import sponsors from "../assets/sponsors.png";
 const Sponsors = () => {
   return (
-    <section className="text-center text-white bg-secondLens py-8 bg-cover bg-center px-5 md:px-20 layout grid justify-center">
+    <section className="text-center relative text-white bg-secondLens py-8 bg-cover bg-center px-5 md:px-20 layout grid justify-center">
       <h1 className="clash text-xl text-[32px] font-bold">
         Partners and Sponsors
       </h1>
@@ -11,12 +11,25 @@ const Sponsors = () => {
         Getlinked Hackathon 1.0 is honored to have the following major companies
         as its partners and sponsors
       </p>
-      <div className="w-full">
-        <img src={sponsors} alt="sponsors" className="" />
-      </div>
-      <img src={purpleStar} alt="Star" />
-      <img src={purpleStar} alt="Star" />
-      <img src={whiteStar} alt="Star" />
+      <img
+        src={purpleStar}
+        alt="Star"
+        className="hidden lg:block w-5 ml-[10%] star"
+      />
+      <div className="w-full flex justify-center">
+        <img
+          src={sponsors}
+          alt="sponsors"
+          className="w-[333px] md:w-[500px] lg:w-[1255px]"
+        />
+      <img
+        src={purpleStar}
+        alt="Star"
+        className="star w-3 lg:hidden absolute"
+        />
+        </div>
+      {/* <img src={purpleStar} alt="Star" className="star w-3 lg:w-5 absolute" />
+      <img src={whiteStar} alt="Star" className="star w-3 lg:w-5 absolute" /> */}
     </section>
   );
 };
